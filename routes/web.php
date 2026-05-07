0e186518f9a5ac91313495b2b85233872ea61ee8
Md Muntahin Akbar Nobo <mdmuntahinakbarnobo@gmail.com>
2026-05-07 11:16:01 +0600

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [TodoController::class, 'index'])->name('todos.index');
+Route::get('/todos/create', [TodoController::class, 'create'])->name('todos.create');
+Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
+Route::get('/todos/{todo}', [TodoController::class, 'show'])->name('todos.show');
+Route::get('/todos/{todo}/edit', [TodoController::class, 'edit'])->name('todos.edit');
+Route::put('/todos/{todo}', [TodoController::class, 'update'])->name('todos.update');
+Route::patch('/todos/{todo}/toggle', [TodoController::class, 'toggleStatus'])->name('todos.toggle');
+Route::delete('/todos/{todo}', [TodoController::class, 'destroy'])->name('todos.destroy');
