5d0932c84b4f025c28443b8f9bc8bd775c7f70ce
Md Muntahin Akbar Nobo <mdmuntahinakbarnobo@gmail.com>
2026-05-07 10:50:20 +0600

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,8 @@
+'mysql' => [
+    'driver' => 'mysql',
+    'host' => env('DB_HOST', '127.0.0.1'),
+    'port' => env('DB_PORT', '3306'),
+    'database' => env('DB_DATABASE', 'laravel_todo'),
+    'username' => env('DB_USERNAME', 'root'),
+    'password' => env('DB_PASSWORD', ''),
+],
