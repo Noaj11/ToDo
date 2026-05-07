a494f11ea6b65cdac1c83e806369625b082dd68f
Md Muntahin Akbar Nobo <mdmuntahinakbarnobo@gmail.com>
2026-05-07 10:41:48 +0600

    Create confiq.php

---
+++ bootstrap/cache/confiq.php
@@ -0,0 +1,21 @@
+<?php
+
+return array (
+  'compiled' => 
+  array (
+    0 => '/Users/youruser/laravel-todo-app/config/app.php',
+    1 => '/Users/youruser/laravel-todo-app/config/auth.php',
+    2 => '/Users/youruser/laravel-todo-app/config/broadcasting.php',
+    3 => '/Users/youruser/laravel-todo-app/config/cache.php',
+    4 => '/Users/youruser/laravel-todo-app/config/cors.php',
+    5 => '/Users/youruser/laravel-todo-app/config/database.php',
+    6 => '/Users/youruser/laravel-todo-app/config/filesystems.php',
+    7 => '/Users/youruser/laravel-todo-app/config/hashing.php',
+    8 => '/Users/youruser/laravel-todo-app/config/logging.php',
+    9 => '/Users/youruser/laravel-todo-app/config/mail.php',
+    10 => '/Users/youruser/laravel-todo-app/config/queue.php',
+    11 => '/Users/youruser/laravel-todo-app/config/sanctum.php',
+    12 => '/Users/youruser/laravel-todo-app/config/session.php',
+    13 => '/Users/youruser/laravel-todo-app/config/view.php',
+  ),
+);
